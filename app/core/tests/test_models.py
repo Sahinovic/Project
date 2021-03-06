96b8cca4905e6513aec258dc1dad304014545ccc
Mirza <sahinovic@gmail.com>
2021-03-07 00:06:57 +0100

    ases tags

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,7 +1,11 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from core.models import UserManager
+from core import models
 
+def sample_user(email='test@gmail.com', password="test1234"):
+    """create sample user """
+    return get_user_model().objects.create_user(email,password)
 class ModelTests(TestCase):
 
     def test_create_user_with_email_successefull(self):
@@ -39,3 +43,13 @@ class ModelTests(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_tag_str(self):
+        """test tag string representatiom"""
+
+        tag = models.Tag.objects.create(
+            user = sample_user(),
+            name='vegan'
+        )
+
+        self.assertEqual(str(tag), tag.name)
